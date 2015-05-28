a6c9e81c5d6dbdaf226879a6d10b1c6cb71f7a17
CupOfTea696 <cupoftea696@gmail.com>
2015-05-28 17:35:16 +0200

    Release: v1.0.0

--- src/FormatTxt.php
+++ src/FormatTxt.php
@@ -15,7 +15,7 @@ class FormatTxt
      * @const string
      */
     const PACKAGE = 'CupOfTea/FormatTxT';
-    const VERSION = '0.0.0';
+    const VERSION = '1.0.0';
     
     /**
      * Beautify a string into paragraphs and clickable links
